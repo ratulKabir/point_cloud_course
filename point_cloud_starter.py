8a1cc223b022b7cde33aab47e8f43e523d5f7934
ratul <raihankabirratul@gmail.com>
2022-01-13 17:47:39 +0100

    challenge 5, 4 solved. yet to do the bonuses. No need for task 6 for now.

--- point_cloud_starter.py
+++ point_cloud_starter.py
@@ -40,19 +40,54 @@ inlier_cloud.paint_uniform_color([0, 1, 1])
 
 end_time = time.time()
 print("Time taken:", end_time-start_time)
-o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+# o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
 
-# ## CHALLENGE 4 - CLUSTERING USING DBSCAN
-# labels =
-# max_label= labels.max()
-# print(f"point cloud has {max_label + 1} clusters")
-#
+## CHALLENGE 4 - CLUSTERING USING DBSCAN
+with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
+    labels = np.array(outlier_cloud.cluster_dbscan(eps=0.2,
+                                         min_points=10,
+                                         print_progress=True))
+
+max_label = labels.max()
+print(f"point cloud has {max_label + 1} clusters")
+colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+colors[labels < 0] = 0
+pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
+# o3d.visualization.draw_geometries([pcd],
+#                                   zoom=0.455,
+#                                   front=[-0.4999, -0.1659, -0.8499],
+#                                   lookat=[2.1813, 2.0619, 2.0999],
+#                                   up=[0.1204, -0.9852, 0.1215])
+
 # ## BONUS CHALLENGE - CLUSTERING USING KDTREE AND KNN INSTEAD
 # pcd_tree =
 #
-# ## CHALLENGE 5 - BOUNDING BOXES IN 3D
-# bounding_boxes =
-#
+## CHALLENGE 5 - BOUNDING BOXES IN 3D
+start_t = time.time()
+obbs = []
+indexes = pd.Series(range(len(labels))).groupby(labels, sort=False).apply(list).tolist()
+
+MAX_POINTS = 300
+MIN_POINTS = 30
+
+for i in range(0, len(indexes)):
+    nb_points = len(outlier_cloud.select_by_index(indexes[i]).points)
+    if (nb_points>MIN_POINTS and nb_points<MAX_POINTS):
+        sub_cloud = outlier_cloud.select_by_index(indexes[i])
+        obb = sub_cloud.get_axis_aligned_bounding_box()
+        obb.color = (0, 0, 1)
+        obbs.append(obb)
+print("Number of bounding boxes: ", len(obbs))
+
+list_of_visuals = []
+list_of_visuals.append(outlier_cloud)
+list_of_visuals.extend(obbs)
+list_of_visuals.append(inlier_cloud)
+
+end_t = time.time()
+
+o3d.visualization.draw_geometries((list_of_visuals))
+
 # ## CHALLENGE 6 - VISUALIZE THE FINAL RESULTS
 # list_of_visuals =
 #
